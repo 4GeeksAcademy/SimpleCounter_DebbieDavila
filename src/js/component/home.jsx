394277765ab30aa24f3a4bb219dd49aabbdc8a48
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-03-08 03:19:29 +0000

    Simple Counter app

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,3 @@
-/*import React from "react";
-
-//create your first component
-const Home = (props) => {
-	let string = "00000" + props.counter
-	console.log(string)
-
-	return (
-		<div className="container" >
-			<div className="secondsCounter box"><i className="far fa-clock"></i></div>
-			<div className="four box"> {string.charAt(string.length - 4)}</div>
-			<div className="three box"> {string.charAt(string.length - 3)}</div>
-			<div className="two box">{string.charAt(string.length - 2)}</div>
-			<div className="one box">{string.charAt(string.length - 1)}</div>
-		</div>
-
-		<div style={{ textAlign: "center", marginTop: "20px" }}>
-				Made with ❤️ by <a href="https://debbiedavila.com/">Deborina Dávila</a>
-			</div>
-			</div>
-	);
-};
-export default Home;*/
-
 import React from "react";
 
 //create your first component
@@ -46,6 +22,7 @@ const Home = (props) => {
 };
 
 export default Home;
+
 
 
 
